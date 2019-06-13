fa1bc8eb71d95b263c6f82c12235d06009d4e5be
gooaah <gaaooh@tom.com>
2019-06-13 18:58:43 +0800

    Gnl,test

--- generator.py
+++ generator.py
@@ -4,7 +4,7 @@ import numpy as np
 def integral_radial(poly1, poly2):
     p = poly1 * poly2 * Polynomial([0,0,1])
     ip = p.integ()
-    I = ip(1)
+    I = ip(1) - ip(0)
     return I
 
 def zernike_dict(nmax):
@@ -14,9 +14,9 @@ def zernike_dict(nmax):
             # print(n,l)
             key = (n,l)
             if l == n:
-                coef = [0 for _ in range(n+1)]
-                coef[n] = 1
-                poly = Polynomial(coef)
+                # coef = [0 for _ in range(n+1)]
+                # coef[n] = 1
+                poly = xn(n)
                 # zerDic[key] = poly
             elif l == n-2:
                 if n >=2:
@@ -27,8 +27,8 @@ def zernike_dict(nmax):
             elif l == 0:
                 n2 = 2*n
                 M1 = (n2+1)*(n2-1)/(n+l+1)/(n-l)
-                M2 = -0.5*(2*l+1)**2*(n2-1) + (n2+1)*(n2-1)*(n2-3)/(n+l+1)/(n-l)/(n2-3)
-                M3 = -1*(n2+1)*(n+l+1)*(n-l-2)/(n+l+1)/(n-l)/(n2-3)
+                M2 = -0.5*((2*l+1)**2*(n2-1) + (n2+1)*(n2-1)*(n2-3))/(n+l+1)/(n-l)/(n2-3)
+                M3 = -1*(n2+1)*(n+l-1)*(n-l-2)/(n+l+1)/(n-l)/(n2-3)
                 poly = (M1*Polynomial([0,0,1]) + M2)*zerDic[(n-2,l)] + M3*zerDic[(n-4,l)]
                 # zerDic[key] = poly
             else:
@@ -52,6 +52,48 @@ def div_xn(poly, n):
     assert np.all(c[:n] == 0)
     return Polynomial(c[n:])
 
+def xn(n):
+    coef = [0 for _ in range(n+1)]
+    coef[n] = 1
+    return Polynomial(coef)
+
+def G1nl_dict(zerDic):
+    G1Dic = {}
+    for key, val in zerDic.items():
+        _, l = key
+        iPoly = val * xn(l+2)
+        iPoly = iPoly.integ()
+        gPoly = div_xn(iPoly, l+1)
+        G1Dic[key] = gPoly
+
+    return G1Dic
+
+def G2nl_dict(zerDic):
+    G2Dic = {}
+    for key, val in zerDic.items():
+        _, l = key
+        if l <= 1:
+            iPoly = val * xn(1-l)
+        else:
+            iPoly = div_xn(val, l-1)
+        iPoly = iPoly.integ()
+        iPoly = -1*iPoly + iPoly(1)
+        gPoly = iPoly * xn(l)
+        G2Dic[key] = gPoly
+
+    return G2Dic
+
+
 
 if __name__ == '__main__':
-    zerDic = zernike_dict(5)
+    nmax = 5
+    zerDic = zernike_dict(nmax)
+    G1Dic = G1nl_dict(zerDic)
+    G2Dic = G2nl_dict(zerDic)
+
+    for (n,l), poly in zerDic.items():
+        print((n,l),integral_radial(poly,poly), 1/(2*n+3))
+
+    for (n,l), poly in zerDic.items():
+        for i in range(n+2, nmax+1, 2):
+            print((n,l),(i,l),integral_radial(poly, zerDic[(i,l)]))
